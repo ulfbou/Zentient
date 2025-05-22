71e2e08aabc8b01286f5a5e6fc315e4075522f21
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-05-22 09:29:36 +0200

    feat(Results.AspNetCore): Add ServiceCollectionExtensions for ProblemDetails integration
    This commit introduces the `ServiceCollectionExtensions` static class, providing an extension method to simplify the integration of `Zentient.Results` ProblemDetails support into ASP.NET Core MVC.
    
    The added extension method is:
    
    -   **`AddZentientResultProblemDetails(this MvcOptions options, Action<ProblemDetailsOptions>? configure = null)`**: This extension method configures MVC options to include the `ProblemDetailsResultFilter` as a global filter. This filter automatically intercepts failed `Zentient.Results.IResult` instances returned from controller actions and transforms them into standardized `ProblemDetails` responses. An optional `configure` action allows for customization of the `ProblemDetailsOptions`, enabling developers to configure how Problem Details responses are generated.
    
    By using this extension method, developers can easily enable automatic Problem Details generation for their `Zentient.Results`-based APIs within their ASP.NET Core MVC applications.

---
+++ Source/Results.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using System;
+
+using Zentient.Results.AspNetCore;
+
+namespace Zentient.Results.AspNetCore
+{
+    /// <summary>
+    /// Provides extension methods for configuring Zentient.Results ProblemDetails integration for ASP.NET Core MVC.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds a global <see cref="ProblemDetailsResultFilter"/> to the MVC options pipeline.
+        /// This filter automatically converts failed <see cref="Zentient.Results.IResult"/> instances
+        /// returned from controller actions into <see cref="ProblemDetails"/> responses.
+        /// </summary>
+        /// <param name="options">The <see cref="MvcOptions"/> to configure.</param>
+        /// <param name="configure">An optional action to configure the <see cref="ProblemDetailsOptions"/>.</param>
+        /// <returns>The <see cref="MvcOptions"/> instance so that additional configuration can be chained.</returns>
+        public static MvcOptions AddZentientResultProblemDetails(this MvcOptions options, Action<ProblemDetailsOptions>? configure = null)
+        {
+            var problemDetailsOptions = new ProblemDetailsOptions();
+            configure?.Invoke(problemDetailsOptions);
+            options.Filters.Add(new ProblemDetailsResultFilter(Options.Create(problemDetailsOptions)));
+            return options;
+        }
+    }
+}
